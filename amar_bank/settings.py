1cfe7b59f4ec3f39c7f2d461447105e581e12592
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2024-04-25 21:50:38 +0600

    update crsf

--- amar_bank/settings.py
+++ amar_bank/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY =env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['amar-bank-esbg.onrender.com']
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://amar-bank-esbg.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
